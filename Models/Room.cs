a4f0dd8517c335c2b207293989993fc5ce1d290e
shehanjay21 <dinushaj156@gmail.com>
2025-02-03 15:58:00 +0530

    Add files via upload

---
+++ Models/Room.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem3.Models
+{
+    internal class Room
+    {
+    }
+}
